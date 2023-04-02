836588379e5b54e2284dbcbee0316539abb36dcb
Qifan Lu <lqf.1996121@gmail.com>
2023-04-01 22:29:00 -0700

    cosmetic improvements

--- images/dl-cluster/pre-start-hook.bash
+++ images/dl-cluster/pre-start-hook.bash
@@ -11,39 +11,29 @@ export PIP_CACHE_DIR="${DL_SHARED_DIR}/cache/pip"
 # Conda environments directories
 export CONDA_ENVS_PATH="${DL_USER_ENVS_DIR}:${DL_SHARED_DIR}/envs/shared"
 
-# Determined settings
-export DET_SKIP_PIP_INSTALL=1
-
-# Jupyter directories
-export JUPYTER_CONFIG_DIR=/run/determined/jupyter/config
-export JUPYTER_DATA_DIR=/run/determined/jupyter/data
-export JUPYTER_RUNTIME_DIR=/run/determined/jupyter/runtime
-
-# NCCL settings
-export NCCL_SOCKET_NTHREADS=2
-export NCCL_NSOCKS_PERTHREAD=6
-
-# Initialize user directories
+# Initialize user data directory
 if [ ! -e "${DL_USER_DATA_DIR}" ]; then
+    # Copy user skeleton directory
     cp -r /etc/skel "${DL_USER_DATA_DIR}"
-fi
-mkdir -p "${DL_USER_ENVS_DIR}"
-# Use user data directory as home directory
-ln -s "${DL_USER_DATA_DIR}" "${HOME}"
-# Create link to shared data directory
-if [ ! -e "${HOME}/shared" ]; then
+    # Use user data directory as home directory
+    ln -s "${DL_USER_DATA_DIR}" "${HOME}"
+    # Create link to shared data directory
     ln -s "${DL_SHARED_DIR}/data/shared" "${HOME}/shared"
+    
+    # Persistently initialize Conda for interactive shells
+    conda init
+    # Display short environment prompt
+    conda config --set env_prompt '({name}) '
+else
+    ln -s "${DL_USER_DATA_DIR}" "${HOME}"
 fi
+# Create user environments directory
+mkdir -p "${DL_USER_ENVS_DIR}"
+
 # Switch to home directory except for experiments
 if [ "${DET_TASK_TYPE}" != "TRIAL" ]; then
     cd "${HOME}"
 fi
 
-# Initialize Conda for current shell
-eval "$(${DL_SHARED_DIR}/conda/bin/conda shell.bash hook)"
-# Persistently initialize Conda for interactive shells
-conda init
-# Display short environment prompt
-conda config --set env_prompt '({name}) '
 # Activate cluster environment
-conda activate "${DL_CLUSTER_ENV}"
+conda activate dl-cluster
